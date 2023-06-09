8c485309e8627546bdc7ec4479cdc5f7b819d060
mithilesh kumar <kumaryadavmithilesh861@gmail.com>
2023-06-09 21:43:31 +0530

    all probloms solve

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import  { Route, Routes }from 'react-router-dom';
 import Home from './pages/Home'
 import Header from './components/Header';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Booking from './pages/Booking';
-import MarrigeLawn from './components/MarrigeLawn';
+import Login from './components/user/Login';
 import Contact from './components/Contact';
-import Admin from './pages/Admin';
-// import Footer from './pages/footer';
+import Error from './pages/Error';
+import Footer from './components/Footer';
+import LoginPages from './components/admin/LoginPages';
+import AdminDashboard from './components/admin/AdminDashboard';
+import Booking from './components/user/Booking';
+import Signup from './components/user/Signup';
+
 function App() {
   return (
     <>
     <Header/>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Booking/" element={<Booking />} />
-      <Route path='/Login' element={<Login/>} />
       <Route path='/Signup' element= {<Signup/>}/>
-      <Route path='/MarrigeLawn' element= {<MarrigeLawn/>}/>
-      <Route path='/Admin' element= {<Admin/>}/>
+      <Route path='/Login' element={<Login/>} />
       <Route path='/Contact' element= {<Contact/>}/>
+
+      <Route path="/Booking/" element={<Booking />} />
+      <Route path="/admin" element={<LoginPages />} />
+      <Route path="/admindashboard" element={<AdminDashboard />} />
+      <Route path="*" element={<Error />} />
     </Routes>
-      {/* <Footer/> */}
+      <Footer/>
   </>
   )
 }
---
+++ src/components/user/Login.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login() {
+  const [details, setDetails] = useState({});
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      console.log('apple');
+      const res = await axios.post(
+        'https://long-shorts-worm.cyclic.app/users/login',
+        details,
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="bg-gradient-to-t from-sky-400 to-cyan-300 flex justify-center items-center min-h-screen">
+  
+      <div className='flex justify-center items-center '>
+        <div className='-mt-20 bg-gray-800/50 py-10 pb-7 px-20 w-96 h-96 bg-gradient-to-t from-sky-400 to-cyan-300  '>
+        <form>
+        <p className='text-center text-xl'>User Login</p>
+        <input
+              onChange={(e) =>
+                setDetails((prevState) => ({
+                  ...prevState,
+                  email: e.target.value,
+                }))
+              }
+              placeholder="Enter Your Email"
+              required
+              type="email"
+              className="font-serif rounded-3xl border-2 border-black text-center w-60 h-10 mt-4"
+            ></input>
+            <input
+              onChange={(e) =>
+                setDetails((prevState) => ({
+                  ...prevState,
+                  password: e.target.value,
+                }))
+              }
+              placeholder="Enter Your Password"
+              required
+              type="password"
+              className="font-serif rounded-3xl border-2 border-black text-center w-60 h-10 mt-2"
+            ></input>
+            <button
+              onClick={onSubmit}
+              type="submit"
+              className="w-36 h-10 rounded-3xl font-serif font-bold text-white hover:bg-yellow-700 bg-blue-900 mt-2 ml-10"
+            >
+              Submit
+            </button>
+        </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
